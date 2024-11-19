a2d9753b0ada10b796b3748978a7170f29db6fb6
Jberestian <jberestian@gmail.com>
2024-11-19 17:37:07 +0200

    added fetch request

--- src/Api.tsx
+++ src/Api.tsx
@@ -1,30 +1,3 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -50,7 +23,7 @@ const App: React.FC = () => {
     const fetchMeal = async () => {
       try {
         const response = await axios.get<{ meals: Meal[] | null }>(
-          "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
+          "https://www.themealdb.com/api/json/v1/1/search.php?f=a"
         );
         const data = response.data.meals;
 
